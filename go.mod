cb52aad06b2622b741ffa6d77610561462aac8f5
Alexandre E Souza <alexandre@dev2learn.com>
2025-06-29 12:14:55 -0300

    update

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/devalexandre/broker-ui
 
 go 1.22
 
-
 require (
 	fyne.io/fyne/v2 v2.5.0
 	github.com/mattn/go-sqlite3 v1.14.22
@@ -18,6 +17,8 @@ require (
 	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
 	github.com/fyne-io/glfw-js v0.0.0-20240101223322-6e1efdc71b7a // indirect
 	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
+	github.com/fynelabs/fyneselfupdate v0.1.1
+	github.com/fynelabs/selfupdate v0.2.0
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
 	github.com/go-text/render v0.1.0 // indirect
--- main.go
+++ main.go
@@ -16,6 +16,8 @@ import (
 	"fyne.io/fyne/v2/widget"
 	"github.com/devalexandre/broker-ui/natscli"
 	"github.com/devalexandre/broker-ui/themes/dracula"
+	"github.com/fynelabs/fyneselfupdate"
+	"github.com/fynelabs/selfupdate"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/nats-io/nats.go"
 )
@@ -649,12 +651,11 @@ func addDashboardTab() {
 	}()
 }
 
-// selfManage turns on automatic updates
 func selfManage(a fyne.App, w fyne.Window) {
-	publicKey := ed25519.PublicKey{119, 74, 124, 79, 146, 202, 6, 214, 121, 135, 49, 80, 146, 127, 88, 29, 133, 69, 58, 115, 234, 47, 160, 80, 17, 102, 7, 188, 131, 198, 213, 43}
+	publicKey := ed25519.PublicKey{200, 37, 164, 131, 164, 172, 52, 181, 239, 251, 200, 30, 190, 92, 215, 209, 174, 6, 144, 222, 75, 0, 52, 75, 52, 11, 58, 59, 217, 7, 46, 75}
 
 	// The public key above matches the signature of the below file served by our CDN
-	httpSource := selfupdate.NewHTTPSource(nil, "https://geoffrey-artefacts.fynelabs.com/self-update/a2/a2486f1c-c255-4ca6-9b37-abf98db85117/{{.OS}}-{{.Arch}}/{{.Executable}}{{.Ext}}")
+	httpSource := selfupdate.NewHTTPSource(nil, "https://geoffrey-artefacts.fynelabs.com/self-update/51/510d1864-0874-460d-bce6-36438c777ed4/{{.OS}}-{{.Arch}}/{{.Executable}}{{.Ext}}")
 
 	config := fyneselfupdate.NewConfigWithTimeout(a, w, time.Minute, httpSource, selfupdate.Schedule{FetchOnStart: true, Interval: time.Hour * 12}, publicKey)
 
